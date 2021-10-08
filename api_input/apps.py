33e85f762cb65075ecd35e0fe0ba3baaa418dfc0
araimbekov <azim28021975>
2021-10-08 22:57:10 +0700

    first steps

---
+++ api_input/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class ApiInputConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'api_input'
---
+++ main/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.postgres.fields import BigIntegerRangeField
+
+# Create your models here.
+class CallDetailRecord(models.Model):
+    """ Основной класс первоначальных CDR """
+
+
+class CountedCallDetailRecord(models.Model):
+    """ Подсчитанные CDR с работником и ценой """
+
+
+class Tarif(models.Model):
+    """ Основной класс тарифов операторов """
+    code = BigIntegerRangeField(blank=True, null=True)
+
+
+class Employee(models.Model):
+    """ Сотрудники предприятия """
+
+
+class Department(models.Model):
+    """ Отделы предприятия """
+
+
+class Company(models.Model):
+    """ Компании предприятия """
